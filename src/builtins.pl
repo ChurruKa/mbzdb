bebb0c9948168d11aac61a4c3c64fdfbc896e7c9
Héctor Arroyo <harroyo@hangar18.cc>
2016-09-12 19:48:13 +0200

    Bump version

--- src/builtins.pl
+++ src/builtins.pl
@@ -2,8 +2,8 @@
 use DBI;
 
 # Version. When changing this also look in languages/ for $L{'init_welcome'}
-$g_version = "7.0";
-$g_build_date = "2015-10-17";
+$g_version = "7.1";
+$g_build_date = "2016-09-12";
 
 # System commands
 $g_mv = (($^O eq "MSWin32") ? "move" : "mv");
